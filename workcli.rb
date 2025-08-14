7272c2a6bf30dda610d8ed0769e15a19f5dffdf8
Mike Lynch <nycynik@gmail.com>
2025-08-14 12:36:02 -0700

    updaing sha

--- workcli.rb
+++ workcli.rb
@@ -2,7 +2,7 @@ class Workcli < Formula
   desc "Simple tool that helps automate developer workflows from the command line in Linux and OSX"
   homepage "https://github.com/nycynik/workcli"
   url "https://github.com/nycynik/workcli/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "b7a952d555b8eb8cb227609fd3a8ab2df15f5177ac49c7abc30df5b0eb0cb091"
+  sha256 "d1eb8e122da5cd87a49f06d1b2878a8fc855ee06af98c35fe745eb733f6e624d"
   license "MIT"
 
   def install
