d73cee259956d7e8c4d70531004a636fb055ddcc
Mike Lynch <nycynik@gmail.com>
2025-08-14 11:46:14 -0700

    releaseing 0.1.1

--- workcli.rb
+++ workcli.rb
@@ -1,8 +1,8 @@
 class workcli < Formula
-  desc "Simple tool that helps automate developer workflows from the command line in linux and OSX"
+  desc "Simple tool that helps automate developer workflows from the command line in Linux and OSX"
   homepage "https://github.com/nycynik/workcli"
-  url "https://github.com/you/mytool/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "<sha256>"
+  url "https://github.com/you/mytool/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "7fc9c67c51a07396f37ac063fe177f5640fa000aad85f1e08888e9d294808cb2"
   license "MIT"
 
   def install
