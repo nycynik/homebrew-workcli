da3a89c175fb3f44f034832d9d796fccd4adf076
Mike Lynch <nycynik@gmail.com>
2025-08-14 12:18:08 -0700

    release updating formula to 0.1.2

--- workcli.rb
+++ workcli.rb
@@ -1,13 +1,14 @@
 class Workcli < Formula
   desc "Simple tool that helps automate developer workflows from the command line in Linux and OSX"
   homepage "https://github.com/nycynik/workcli"
-  url "https://github.com/nycynik/workcli/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "00acd467d410f2e6401a5de48c21acb2402764d85dde42ef375d6e606ef4497e"
+  url "https://github.com/nycynik/workcli/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "90b1e9f049f6480f2c6ce6a6dd39573f6e6151168eb01a53118bf8af1fad6781"
   license "MIT"
 
   def install
-    bin.install "bin/workcli"
     libexec.install Dir["lib/*"]
+    bin.install "bin/workcli"
+    (bin/"workcli").write_env_script libexec/"../bin/workcli", WORKCLI_LIBEXEC: libexec
   end
 
   test do
